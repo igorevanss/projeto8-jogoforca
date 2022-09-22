f4a775a20cc65a53d8814eea2688062ce814b24c
igorevanss <igorevans.pro@gmail.com>
2022-09-22 17:33:35 -0300

    loǵica de embaralgar e escolher uma palavra implementada, traços que aparecerão indicando quantas letras a palavra tem construídos.

--- src/components/App.js
+++ src/components/App.js
@@ -1,4 +1,5 @@
-import '../assets/style.css';
+import '../assets/style.css'
+import words from './Words'
 import forca0 from '../assets/images/forca0.png'
 import forca1 from '../assets/images/forca1.png'
 import forca2 from '../assets/images/forca2.png'
@@ -6,19 +7,61 @@ import forca3 from '../assets/images/forca3.png'
 import forca4 from '../assets/images/forca4.png'
 import forca5 from '../assets/images/forca5.png'
 import forca6 from '../assets/images/forca6.png'
+import { useState } from 'react'
 
 function Letter(props) {
   const { letter } = props
+
+  const [desable, setDesable] = useState('letter')
+
+  function desableBtn() {
+    setDesable('letter-desabled')
+  }
+
   return (
-    <div onClick={''} className="letter">
+    <div onClick={desableBtn} className={desable}>
       <p>{letter}</p>
     </div>
   )
 }
 
-const alphabet = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"]
+const alphabet = [
+  'a',
+  'b',
+  'c',
+  'd',
+  'e',
+  'f',
+  'g',
+  'h',
+  'i',
+  'j',
+  'k',
+  'l',
+  'm',
+  'n',
+  'o',
+  'p',
+  'q',
+  'r',
+  's',
+  't',
+  'u',
+  'v',
+  'w',
+  'x',
+  'y',
+  'z'
+]
 
 export default function App() {
+  const chosenWord = words[Math.floor(Math.random() * words.length)]
+  console.log(chosenWord)
+  const [enable, setEnable] = useState('')
+
+  function enableBtn() {
+    setEnable()
+  }
   return (
     <div className="container">
       <main>
@@ -28,6 +71,10 @@ export default function App() {
           <button onClick={''}>
             <p>Escolher Palavra</p>
           </button>
+          <div className="word"></div>
+          <div className="word"></div>
+          <div className="word"></div>
+          <div className="word"></div>
           <div className="word"></div>
         </div>
       </main>
@@ -40,7 +87,7 @@ export default function App() {
 
       <div className="guess-word">
         <p>Já sei a palavra!</p> <input type="text" />{' '}
-        <button onClick={''}>
+        <button>
           <p>Chutar</p>
         </button>
       </div>
